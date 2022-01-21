da415caefed3241c0468d6849569f4d7c2002ee7
a113942 <88109350+a113942@users.noreply.github.com>
2022-01-21 16:12:11 -0500

    Initial commit

---
+++ CSharpExamples/ConvertingTemperatures.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+using Xunit;
+
+namespace CSharpExamples
+{
+    public class ConvertingTemperatures
+    {
+      
+        [Theory]
+        [InlineData(212, 100.01F)]
+        [InlineData(32, 0)]
+        [InlineData(98, 36.67F)]
+        public void CanConvertFromFToC(float tempInF, float expected)
+        {
+            // given
+            // create an instance of the class
+            var converter = new TemperatureConverter();
+
+            // when
+            float tempInC = converter.ConvertToC(tempInF);
+
+            // then
+           Assert.Equal<float>(expected, tempInC);
+        }
+    }
+}
